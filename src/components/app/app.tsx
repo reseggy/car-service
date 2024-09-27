582d2c1f3010326c9144bf4b0731bfde6aee88bf
Alexandr Bukovcev <reseggy@gmail.com>
2024-09-27 23:42:37 +0300

    Сделан компонент header и начать homePage

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,7 @@
 import '../../index.css';
 import styles from './app.module.css';
 import { Routes, Route } from 'react-router-dom';
-import { MainPage } from '../../pages/mainPage';
+import { HomePage } from '../../pages/home-page';
 import { AppHeader } from '../app-header';
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
     <div className={styles.app}>
       <AppHeader />
       <Routes>
-        <Route path='/' element={<MainPage />} />
+        <Route path='/' element={<HomePage />} />
       </Routes>
     </div>
   );
